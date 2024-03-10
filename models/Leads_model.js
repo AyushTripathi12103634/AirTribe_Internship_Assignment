67e2b7bff3b6016917c5d3c130bf978aa8165441
Ayush Kumar Tripathi <ayushtripathi2003@gmail.com>
2024-03-10 16:38:21 +0530

    Created leads model for database

---
+++ models/Leads_model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const leadsschema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    registered_courses: {
+      type: Array,
+      required: true,
+      default: [],
+    },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    linkedin : {
+      type: String,
+      unique: true,
+    },
+    comments: {
+      type: Array,
+      required: true,
+      default: [],
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Leads", leadsschema);
