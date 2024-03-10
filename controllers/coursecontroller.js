a84ac856a65be61f00e1871efb53ff56deb4d589
Ayush Kumar Tripathi <ayushtripathi2003@gmail.com>
2024-03-10 17:51:56 +0530

    Added update lead status api

--- controllers/coursecontroller.js
+++ controllers/coursecontroller.js
@@ -186,3 +186,81 @@ export const updateCourseController = async (req, res) => {
     }
 }
 
+
+// Function to update leads for a course
+export const updateLeadsController = async (req, res) => {
+    try {
+        // Destructure the request body
+        const { accepted_leads, rejected_leads, waiting_leads } = req.body;
+
+        // Extract the token from the request headers
+        const token = req.headers.authorization;
+
+        // Decode the token to get the instructor's ID
+        const id = decode(token)._id;
+
+        // Extract the course ID from the request parameters
+        const courseId = req.params.id;
+
+        // Find the instructor in the database
+        const instructor = await InstructorModel.findOne({ _id: id });
+
+        // If the instructor does not exist, return an error
+        if (!instructor) {
+            return res.status(401).send({
+                success: false,
+                message: "No Instructor found"
+            });
+        }
+
+        try {
+            // Find the course in the database
+            let course = await CourseModel.findOne({ _id: courseId });
+
+            // If the course does not exist, return an error
+            if (!course) {
+                return res.status(404).send({
+                    success: false,
+                    message: "Course not found"
+                });
+            }
+
+            // If the course was not created by the instructor, return an error
+            if (course.createdBy != id) {
+                return res.status(401).send({
+                    success: false,
+                    message: "Unauthorized Access for Course Updation"
+                });
+            }
+
+            // Update the leads for the course
+            course.accepted_leads = accepted_leads;
+            course.rejected_leads = rejected_leads;
+            course.waiting_leads = waiting_leads;
+
+            // Save the updated course
+            await course.save();
+
+            // Return a success message
+            return res.status(200).send({
+                success: true,
+                message: "Leads updated successfully",
+                course
+            });
+        } catch (error) {
+            // If there is an error in updating the leads, return an error message
+            return res.status(400).send({
+                success: false,
+                message: "Leads not updated",
+                error
+            });
+        }
+    } catch (error) {
+        // If there is an error in the API, return an error message
+        return res.status(500).send({
+            success: false,
+            message: "Error in updateLeads API",
+            error
+        });
+    }
+}
